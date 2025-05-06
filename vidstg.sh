945d4bf8a6263e49bab9a8e839c94c387a8308a4
EvanStefan <steve@actlife.gr>
2025-05-06 13:00:32 +0300

    Minor changes

---
+++ vidstg.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+## TRAINING
+python3 -m torch.distributed.launch \
+    --nproc_per_node=8 \
+    scripts/train_net.py \
+    --config-file "experiments/vidstg.yaml" \
+    INPUT.RESOLUTION 420 \
+    OUTPUT_DIR output/vidstg \
+    TENSORBOARD_DIR output/vidstg \
+    | tee tee_log_train.txt
+
