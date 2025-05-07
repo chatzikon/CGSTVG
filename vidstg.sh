2267f19a67966969b21dbaab3a43c5183cddabc4
EvanStefan <steve@actlife.gr>
2025-05-07 09:52:46 +0300

    Increase gpus required to 2

--- vidstg.sh
+++ vidstg.sh
@@ -2,7 +2,7 @@
 
 ## TRAINING
 python3 -m torch.distributed.launch \
-    --nproc_per_node=1 \
+    --nproc_per_node=2 \
     scripts/train_net.py \
     --config-file "experiments/vidstg.yaml" \
     INPUT.RESOLUTION 100 \
